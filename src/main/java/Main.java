623df623cc7597534393387165edf06f919f93bc
ma-qunhui <792277389@qq.com>
2025-09-16 23:24:14 +0800

    使用jmccc启动游戏

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HelloBlockLauncher");
 
+
         // 获取本机mc路径
         String minecraftDir = System.getenv("APPDATA") + "\\.minecraft";
-
         try {
             GameLauncher.gameLauncher("1.21.8", minecraftDir);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
         FirstPage.launchUi(args);
     }
 }
--- src/main/java/org/bkl/game/GameLauncher.java
+++ src/main/java/org/bkl/game/GameLauncher.java
@@ -1,65 +1,30 @@
 package org.bkl.game;
 
+
+import org.to2mbn.jmccc.auth.OfflineAuthenticator;
+import org.to2mbn.jmccc.launch.Launcher;
+import org.to2mbn.jmccc.launch.LauncherBuilder;
+import org.to2mbn.jmccc.option.LaunchOption;
+import org.to2mbn.jmccc.option.MinecraftDirectory;
+
 /**
  * @author LongWei
  * @date 2025/9/15 12:10
  */
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class GameLauncher {
 
     public static void gameLauncher(String version, String minecraftDir) throws Exception {
 
-        String classpath = ClasspathBuilder.buildClasspath(version, minecraftDir);
+        Launcher launcher = LauncherBuilder.buildDefault();
 
-        File classpathFile = new File("classpath.txt");
+        MinecraftDirectory minecraftDirectory = new MinecraftDirectory(minecraftDir);
 
-        // 构建完整的命令列表
-        List<String> command = new ArrayList<>();
-        command.add("java");
-        command.add("-Xmx2G");
-        command.add("-Djava.library.path=" + minecraftDir + "/versions/" + version + "/" + version + "-natives");
-        command.add("-jar");
-        command.add("G:\\BlockLauncher\\BlockLauncher\\BlockLauncher\\minecraft-1.21.8-merged.jar");
-        command.add("net.minecraft.client.main.Main");
-        command.add("--username");
-        command.add("Player");
-        command.add("--version");
-        command.add(version);
-        command.add("--gameDir");
-        command.add(minecraftDir);
-        command.add("--assetsDir");
-        command.add(minecraftDir + "/assets");
-        command.add("--assetIndex");
-        command.add("26");
-        command.add("--uuid");
-        command.add("00000000-0000-0000-0000-000000000000");
-        command.add("--accessToken");
-        command.add("null");
-        command.add("--userType");
-        command.add("legacy");
+        LaunchOption option
+                = new LaunchOption("1.21.8", new OfflineAuthenticator("Id"), minecraftDirectory);
 
-        System.out.println("执行命令: " + String.join(" ", command));
+        launcher.launch(option);
 
-        if (command.isEmpty()) {
-            throw new IllegalStateException("命令列表不能为空");
-        }
-
-        ProcessBuilder pb = new ProcessBuilder(command);
-        pb.redirectErrorStream(true);
-        Process process = pb.start();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-
-        int exitCode = process.waitFor();
-        System.out.println("进程退出代码: " + exitCode);
     }
 }
