ce9f3ca9ddf0e1ace6d49edde4c63a11d9ebb8fb
LongW <792277389@qq.com>
2025-09-22 23:24:49 +0800

    账号设置图形界面

--- src/main/java/org/bkl/ui/LeftVersionPane.java
+++ src/main/java/org/bkl/ui/LeftVersionPane.java
@@ -28,37 +28,53 @@ public class LeftVersionPane {
                 -fx-font-weight: 500;
                 """);
         parent.getChildren().add(accountLabel);
-        Separator separator = new Separator();
-        separator.setStyle("""
+        Separator separator1 = new Separator();
+        separator1.setStyle("""
                 -fx-background-color: #000000;
                 """);
-        parent.getChildren().add(separator);
+        parent.getChildren().add(separator1);
 
         HBox avatarHBox = new HBox();
-        avatarHBox.setPrefSize(200, 100);
+        avatarHBox.setPrefSize(200, 80);
         avatarHBox.setAlignment(Pos.CENTER);
         avatarHBox.setStyle("""
-                -fx-background-color: pink;
                 -fx-padding: 10 10 10 10;
                 """);
+        avatarHBox.setOnMouseEntered(e -> {
+            avatarHBox.setStyle("""
+                    -fx-background-color: rgba(0, 0, 0, 0.1);
+                    """);
+        });
+        avatarHBox.setOnMouseExited(e -> {
+            avatarHBox.setStyle("""
+                    -fx-background-color: transparent;
+                    """);
+        });
         VBox avatarContainer = new VBox(1);
-        avatarContainer.setPrefSize(50, 50);
-        avatarContainer.setMaxSize(50, 50);
-        avatarContainer.setMinSize(50, 50);
+        avatarContainer.setPrefSize(35, 35);
+        avatarContainer.setMaxSize(35, 35);
+        avatarContainer.setMinSize(35, 35);
         avatarContainer.setStyle("""
-                -fx-background-color: skyblue;
+                -fx-background-image: url("image/avatar/avatar1.png");
+                -fx-background-size: cover;
                 """);
         avatarHBox.getChildren().add(avatarContainer);
         VBox accountContainer = new VBox();
-        accountContainer.setPrefSize(100, 100);
-        accountContainer.setMaxSize(100, 100);
-        accountContainer.setMinSize(100, 100);
-        VBox.setMargin(accountContainer, new Insets(0, 0, 0 ,10));
+        accountContainer.setPrefSize(120, 35);
+        accountContainer.setMaxSize(120, 35);
+        accountContainer.setMinSize(120, 35);
         accountContainer.setStyle("""
-                -fx-background-color: blue;
-                
+                -fx-padding: 0 0 0 10;
                 """);
         avatarHBox.getChildren().add(accountContainer);
+        Label accountName = new Label("没有游戏账户");
+        accountName.setStyle("""
+                -fx-font-size: 14px;
+                -fx-font-weight: 800;
+                """);
+        Label addAccount = new Label("点击添加游戏账户");
+        accountContainer.getChildren().addAll(accountName, addAccount);
+
         parent.getChildren().addAll(avatarHBox);
     }
 
