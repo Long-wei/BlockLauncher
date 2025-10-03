14a3f3fb78542f5eb7ff4a303c763bec7cd83cb6
ma-qunhui <792277389@qq.com>
2025-10-03 19:02:54 +0800

    检查游戏版本框架
    Signed-off-by: ma-qunhui <792277389@qq.com>

--- src/main/java/org/bkl/game/VersionParser.java
+++ src/main/java/org/bkl/game/VersionParser.java
@@ -11,6 +11,12 @@ import java.nio.file.Paths;
  */
 public class VersionParser {
 
+    /**
+     * 根据游戏版本获取游戏版本json路径
+     * @param version
+     * @return
+     * @throws Exception
+     */
     public static JSONObject versionParser(String version) throws Exception{
         String versionFilePath = System.getenv("APPDATA") + "\\.minecraft\\versions\\"+ version + "\\" + version + ".json";
         String jsonContent = new String(Files.readAllBytes(Paths.get(versionFilePath)));
---
+++ src/main/java/org/bkl/modloader/FabricChecker.java
@@ -0,0 +1,24 @@
+package org.bkl.modloader;
+
+/**
+ * @author LongWei
+ * @date 2025/10/3 14:57
+ */
+public class FabricChecker {
+
+    public FabricChecker() {
+    }
+
+    /**
+     * 检查指定版本是否已经安装fabric
+     * @param mcVersion 检查版本
+     * @return 已经安装fabric
+     */
+    public static boolean isFabricInstall(String mcVersion) {
+        // 检查游戏版本文件夹
+
+        return false;
+    }
+
+
+}
